631a1b2b7e20d26c21491fb06642cce8c8a797d0
Hiroshi <sephirothMH@outlook.com>
2021-05-31 16:51:49 -0500

    Cambio de nombres y Generator.py a la mitad

---
+++ HowToUseItTests/FakerBooks.py
@@ -0,0 +1,6 @@
+from faker import Faker
+from Core.Clases import libros as libro
+
+"""Aqui se iran generando los datos del libro con Faker"""
+def generar(self):
+    return libro
---
+++ Puertos/Generator/Generator.py
@@ -0,0 +1,15 @@
+from faker import Faker
+from Core.Clases import libros
+from pymongo import MongoClient
+
+cliente = MongoClient()
+
+def Generar1(self):
+    return libros
+    pass
+
+def generarMuchos(self):
+    """Cambiar de acuerdo a la cantidad de libros a generar"""
+    total = 1000
+    for i in range(1,total):
+        pass
